d6850b9a34545c5332af94d60cc0baa675444090
omri <omriarad3@gmail.com>
2015-12-08 20:11:42 +0200

    Fixing task 6 for CR

--- Task_06/hello_world.c
+++ Task_06/hello_world.c
@@ -10,44 +10,30 @@
 #include <linux/uaccess.h>
 
 #define INVALID_INPUT (-1)
-#define MAX_CHARS (100)
-#define STOP_READ (1)
-#define MY_ID ("91bc4039c624")
-#define ID_LEN (strlen(MY_ID))
+#define MY_ID ("91bc4039c624\n")
+#define ID_LEN (13)
 
-static int eudyptula_open(struct inode *inode, struct file *file);
-static int eudyptula_close(struct inode *inodep, struct file *filp);
 static ssize_t eudyptula_write(struct file *file, const char __user *buf,
 				size_t len, loff_t *offset);
 static ssize_t eudyptula_read(struct file *file, char __user *buf,
 				size_t len, loff_t *offset);
 
+static struct miscdevice eudyptula;
+
+
 static const struct file_operations eudyptula_fops = {
 	.write			= eudyptula_write,
 	.read			= eudyptula_read,
-	.open			= eudyptula_open,
-	.release		= eudyptula_close,
 };
 
-static struct miscdevice eudyptula;
-
-static int eudyptula_open(struct inode *inode, struct file *file)
-{
-	pr_info("Device opened\n");
-	return 0;
-}
-
-static int eudyptula_close(struct inode *inodep, struct file *filp)
-{
-	pr_info("Device closed\n");
-	return 0;
-}
 
 static ssize_t eudyptula_write(struct file *file, const char __user *buf,
 				size_t len, loff_t *offset)
 {
-	if (strncmp(buf, MY_ID, ID_LEN) == 0)
-		return strlen(buf);
+	char input[ID_LEN];
+	simple_write_to_buffer(input, ID_LEN - 1, offset, buf, len);
+	if (strncmp(input, MY_ID, ID_LEN - 1) == 0)
+		return ID_LEN;
 
 	return INVALID_INPUT;
 }
@@ -55,32 +41,22 @@ static ssize_t eudyptula_write(struct file *file, const char __user *buf,
 static ssize_t eudyptula_read(struct file *file, char __user *buf,
 				size_t len, loff_t *offset)
 {
-	int i;
-
-	if (*offset != 0)
-		return 0;
-
-	for (i = 0; i < ID_LEN; i++)
-		put_user(MY_ID[i], buf++);
-
-	*offset = STOP_READ;
-	return ID_LEN;
+	return simple_read_from_buffer(buf, len, offset, MY_ID, ID_LEN);
 }
+
+
+static struct miscdevice eudyptula = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "eudyptula",
+	.fops = &eudyptula_fops,
+};
 
 static int __init enter_module(void)
 {
 	int retval;
-
-	eudyptula.minor = MISC_DYNAMIC_MINOR;
-	eudyptula.name = "eudyptula";
-	eudyptula.fops = &eudyptula_fops;
 	retval = misc_register(&eudyptula);
-	if (retval)
-		return retval;
-
-	pr_info("got minor %i\n", eudyptula.minor);
 	pr_debug("Hello World!\n");
-	return 0;
+	return retval;
 }
 
 static void __exit exit_module(void)
