e68c93e81b50c347b3611aa9f92d34bb56a878b3
omri <omriarad3@gmail.com>
2015-12-08 20:08:33 +0200

    Fixing task 5 for CR

--- Task_05/hello_world.c
+++ Task_05/hello_world.c
@@ -14,40 +14,15 @@ static struct usb_device_id keyboard_id_table[] = {
 
 MODULE_DEVICE_TABLE(usb, keyboard_id_table);
 
-static int keyboard_probe(struct usb_interface *interface,
-	const struct usb_device_id *id)
-{
-	pr_err("Hello World!\n");
-	return 0;
-}
-
-static void keyboard_disconnect(struct usb_interface *interface)
-{
-	pr_err("Goodbye cruel world!\n");
-}
-
-static struct usb_driver keyboard_driver = {
-	.name  =	"hello_world",
-	.probe =	keyboard_probe,
-	.disconnect =	keyboard_disconnect,
-	.id_table =	keyboard_id_table,
-};
-
 static int __init enter_module(void)
 {
-	int retval = 0;
-
-	pr_debug("<%s:%s:%d>\n", __FILE__, __func__, __LINE__);
-	retval = usb_register(&keyboard_driver);
-	if (retval)
-		pr_err("usb_register failed with error %d", retval);
-	return retval;
+	pr_debug("Hello World!\n");
+	return 0;
 }
 
 static void __exit exit_module(void)
 {
-	pr_debug("<%s:%s:%d>\n", __FILE__, __func__, __LINE__);
-	usb_deregister(&keyboard_driver);
+	pr_debug("Goodbye cruel world!\n");
 }
 
 module_init(enter_module);
